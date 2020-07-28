916bbf01fab2877234776f70205d981633e33511
christianprado911 <christian.fisica@gmail.com>
2020-07-28 16:58:17 +0000

    modified:   host_rpmsg_mcp3208.c 	new file:   teste.txt

--- host_rpmsg_mcp3208.c
+++ host_rpmsg_mcp3208.c
@@ -12,8 +12,8 @@
 uint8_t readBuf[512];
 #define DEVICE_NAME  "/dev/rpmsg_pru1"
 
-#define NUM_SCAN_ELEMENTS  6
-#define NUM_SCANS          40
+#define NUM_SCAN_ELEMENTS  4
+#define NUM_SCANS          60
 #define DATA_BUFFER_LEN    (NUM_SCAN_ELEMENTS * NUM_SCANS)
 #define BUFFER_WORD_LEN    2 + DATA_BUFFER_LEN/2
 
@@ -21,6 +21,8 @@ typedef struct {
   uint64_t timestamp_ns;
   uint16_t data[DATA_BUFFER_LEN];
 } Buffer;
+
+  uint16_t ch[NUM_SCAN_ELEMENTS][NUM_SCANS];
 
 int main(void) {
   /* Open the rpmsg_pru character device file */
@@ -30,25 +32,30 @@ int main(void) {
     return -1;
   }
 
+  FILE *fp;
+  fp = fopen("/home/debian/teste.txt", "a");
+
   uint64_t last_ts = 0;
-  for(;;) {
+  for(int j=0; j<3;j++) {
     /* Kick the PRU through the RPMsg channel */
     int result = write(fd, 0, 0);
     if (result < 0) {
       perror("Error writing to PRU");
       return -1;
     }
-    
     // Read the data from the PRU.
     result = read(fd, readBuf, sizeof readBuf);
     if (result == sizeof(Buffer)) {
       Buffer *b = (Buffer *) readBuf;
       for (int i=0; i<DATA_BUFFER_LEN; i++){
-        printf("ch%d=%4" PRIu16 ", ", i % NUM_SCAN_ELEMENTS, b->data[i]);
-        if(i % NUM_SCAN_ELEMENTS == NUM_SCAN_ELEMENTS - 1){
-      printf("ts=%" PRIu64 ",\t", b->timestamp_ns);
-      printf("delta=%" PRIu64 ",\n", b->timestamp_ns - last_ts);
-      }}
+      ch[i % NUM_SCAN_ELEMENTS][i % NUM_SCANS] = b->data[i];
+        fprintf(fp, "ch%d=%4" PRIu16 ", ", i % NUM_SCAN_ELEMENTS , ch[i % NUM_SCAN_ELEMENTS][i % NUM_SCANS]);
+      if(i % NUM_SCAN_ELEMENTS == NUM_SCAN_ELEMENTS - 1){
+      fprintf(fp, "ts=%" PRIu64 ",\t", b->timestamp_ns);
+      fprintf(fp, "delta=%" PRIu64, b->timestamp_ns - last_ts);
+      fprintf(fp, "\n");}
+
+      }
       last_ts = b->timestamp_ns;
     } else if (result < 0) {
       perror("Error reading from device");
@@ -63,3 +70,4 @@ int main(void) {
   close(fd);
   return 0;
 }
+
