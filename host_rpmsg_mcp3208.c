ab5e51083f3f6818e36d0490f6dd0c21caa7e21b
christianprado911 <52748683+christianprado911@users.noreply.github.com>
2020-08-11 08:34:16 -0300

    Update host_rpmsg_mcp3208.c

--- host_rpmsg_mcp3208.c
+++ host_rpmsg_mcp3208.c
@@ -49,13 +49,12 @@ int main(void) {
       Buffer *b = (Buffer *) readBuf;
       for (int i=0; i<DATA_BUFFER_LEN; i++){
         fprintf(fp, "ch%d=%4" PRIu16 ", ", i % NUM_SCAN_ELEMENTS , b->data[i]);
-     if(i % NUM_SCAN_ELEMENTS == NUM_SCAN_ELEMENTS - 1)
+      if(i % NUM_SCAN_ELEMENTS == NUM_SCAN_ELEMENTS - 1)
        fprintf(fp, "\n");
       }
-      last_ts = b->timestamp_ns;
         fprintf(fp, "ts=%" PRIu64 ",  ", b->timestamp_ns);
-        fprintf(fp, "delta=%" PRIu64, b->timestamp_ns - last_ts);
-        fprintf(fp, "\n");
+        fprintf(fp, "delta=% \n" PRIu64, b->timestamp_ns - last_ts);
+     last_ts = b->timestamp_ns;
     } else if (result < 0) {
       perror("Error reading from device");
       return -1;
